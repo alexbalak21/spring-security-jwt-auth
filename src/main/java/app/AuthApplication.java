ed5094c7dfff6a9311a3bf8e6c0cadcd2f9625fd
Alex Balak <alex.balak@outlook.com>
2025-06-19 11:36:50 +0200

    Created HomeController

--- src/main/java/app/AuthApplication.java
+++ src/main/java/app/AuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityJwtAuthApplication {
+public class AuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtAuthApplication.class, args);
+		SpringApplication.run(AuthApplication.class, args);
 	}
 
 }
--- src/test/java/app/AuthApplicationTests.java
+++ src/test/java/app/AuthApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringSecurityJwtAuthApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
