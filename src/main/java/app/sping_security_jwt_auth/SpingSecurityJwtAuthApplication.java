0d3177d209989142fb7464d844b68e0c3cdea2f6
Alex Balak <alex.balak@outlook.com>
2025-06-20 11:35:04 +0200

    Initial Spring Project with Java 24

---
+++ src/main/java/app/sping_security_jwt_auth/SpingSecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package app.sping_security_jwt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingSecurityJwtAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingSecurityJwtAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/sping_security_jwt_auth/SpingSecurityJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package app.sping_security_jwt_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingSecurityJwtAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
